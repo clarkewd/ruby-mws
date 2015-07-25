e8aeb1fbd96c3ac36f8d9a6e6adfd35960120d84
clarkewd <clarkewd@users.noreply.github.com>
2015-07-24 20:08:57 -0400

    disable debug_output

--- lib/ruby-mws/api/base.rb
+++ lib/ruby-mws/api/base.rb
@@ -7,7 +7,7 @@ module MWS
     class Base
       include HTTParty
       parser MWS::API::BinaryParser
-      debug_output $stderr if Rails.env.development? # only in development
+      # debug_output $stderr if Rails.env.development? # only in development
       #format :xml
       headers "User-Agent"   => "ruby-mws/#{MWS::VERSION} (Language=Ruby/1.9.3-p0)"
       headers "Content-Type" => "text/xml"
